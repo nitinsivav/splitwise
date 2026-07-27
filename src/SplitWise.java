d493bbbd6766972ec3fa0a861f2bc3271a0eff94
nitinsivav <nitinsiva.v@gmail.com>
2026-07-27 19:30:41 +0530

    asi-1.1

---
+++ src/SplitWise.java
@@ -0,0 +1,6 @@
+public class SplitWise {
+    public static void main(String[] args) {
+        System.out.println("=== Splitwise App ===");
+        System.out.println("Track Shared Shared Expense");
+    }
+}
