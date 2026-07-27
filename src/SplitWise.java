b028ff8d0d11fe4c0fb6c5f48d3dafe25ab4a454
nitinsivav <nitinsiva.v@gmail.com>
2026-07-27 19:32:33 +0530

    asi-1.2

--- src/SplitWise.java
+++ src/SplitWise.java
@@ -1,6 +1,12 @@
 public class SplitWise {
     public static void main(String[] args) {
+
+        double totalAmount = 500;
+        int numFriends = 3;
+
         System.out.println("=== Splitwise App ===");
         System.out.println("Track Shared Shared Expense");
+        System.out.println(totalAmount);
+        System.out.println(numFriends);
     }
 }
