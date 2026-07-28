e51602778532b8f26fd9f89fde91f58539abcbde
Nitin Siva V <nitinsiva.v@gmail.com>
2026-07-28 11:05:46 +0530

    as-1.7

--- src/SplitWise.java
+++ src/SplitWise.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 public class SplitWise {
     public static void main(String[] args) {
-        Scanner sc = new Scanne(System.in);
+        Scanner sc = new Scanner(System.in);
         int numFriends = 3;
-        double sharePerPerson = totalAmount/numFriend;
 
         System.out.println("=== Splitwise App ===");
         System.out.println("Track Shared Shared Expense");
+        boolean run = true;
+        while(run){
+        System.out.println("1. Record");
+        System.out.println("0. Exit");
+        int ch = sc.nextInt();
+        switch(ch){
+            case 1->{
         System.out.print("Payer Name : ");
         String payerName = sc.nextLine();
         System.out.print("Total Amount :");
-        double totalAmount = sc.nextLine();
+        double totalAmount = sc.nextDouble();
+        double sharePerPerson = totalAmount/numFriends;
 
         String shareMsg = "Each person should pay %.2f".formatted(sharePerPerson);
         String expenseMsg = "%s paid %.2f".formatted(payerName,totalAmount);
@@ -18,5 +25,12 @@ public class SplitWise {
         System.out.println(numFriends);
         System.out.println(expenseMsg);
         System.out.println(shareMsg);
+            }
+            case 0 -> {
+                System.out.println("Goodbye...");
+                run = false;
+            }
+        }
+        }
     }
 }
