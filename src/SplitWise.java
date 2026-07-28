292722d4a7e32e32df0efbb19f20abbd57dbe274
nitinsivav <nitinsiva.v@gmail.com>
2026-07-28 19:21:57 +0530

    as-1.9

--- src/SplitWise.java
+++ src/SplitWise.java
@@ -1,6 +1,19 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class SplitWise {
+
+    public static void addFriends(ArrayList<String> friends, String friendName) {
+        friends.add(friendName);
+        System.out.println("%s added to friends list.".formatted(friendName));
+    }
+
+    public static void listFriends(ArrayList<String> friends) {
+        System.out.println("=== Friends List ===");
+        for (String friend : friends) {
+            System.out.println(" - " + friend);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> friends = new ArrayList<>();
@@ -14,36 +27,46 @@ public class SplitWise {
             System.out.println("3. List Friends");
             System.out.println("0. Exit");
             int ch = sc.nextInt();
+            sc.nextLine();
+
             switch(ch){
                 case 1->{
-                System.out.print("Payer Name : ");
-                String payerName = sc.nextLine();
-                System.out.print("Total Amount :");
-                double totalAmount = sc.nextDouble();
-                double sharePerPerson = totalAmount/friends.size();
+                    
+                    if(friends.size() == 0){
+                        System.out.println("No friends added yet. Please add friends first.");
+                        break;
+                    }
 
-                String shareMsg = "Each person should pay %.2f".formatted(sharePerPerson);
-                String expenseMsg = "%s paid %.2f".formatted(payerName,totalAmount);
-                
-                System.out.println(friends.size());
-                System.out.println(expenseMsg);
-                System.out.println(shareMsg);
+                    System.out.print("Payer Name : ");
+                    String payerName = sc.nextLine();
+                    System.out.print("Total Amount :");
+                    double totalAmount = sc.nextDouble();
+                    double sharePerPerson = totalAmount/friends.size();
+
+                    String shareMsg = "Each person should pay %.2f".formatted(sharePerPerson);
+                    String expenseMsg = "%s paid %.2f".formatted(payerName,totalAmount);
+                    
+                    System.out.println(friends.size());
+                    System.out.println(expenseMsg);
+                    System.out.println(shareMsg);
             }
-            case 2 -> {
-                System.out.print("Enter Friend Name : ");
-                String friendName = sc.nextLine();
-                friends.add(friendName);
-                System.out.println("%s added to friends list.".formatted(friendName));
+                case 2 -> {
+                    System.out.print("Enter Friend Name : ");
+                    String friendName = sc.nextLine();
+                    addFriends(friends, friendName);
             }
-            case 3 -> {
-                System.out.println("Friends List:");
-                for (String friend : friends) {
-                    System.out.println(friend);
+                case 3 -> {
+
+                    if(friends.size() == 0){
+                        System.out.println("No friends added yet. Please add friends first.");
+                        break;
+                    }
+
+                    listFriends(friends);
                 }
-            }
-            case 0 -> {
-                System.out.println("Goodbye...");
-                run = false;
+                case 0 -> {
+                    System.out.println("Goodbye...");
+                    run = false;
             }
         }
         }
